3b1033d043b2b62bbd34a65beff83346f5832167
styx11 <1654630248@qq.com>
2018-09-05 10:16:06 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <back-ground></back-ground>
+  </div>
+</template>
+
+<script>
+import BackGround from './components/BackGround.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BackGround: BackGround
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/BackGround.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="bgImg"></div>
+    <div class="bgCover"></div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'BackGround',
+  data: function () {
+    return {
+      bingImg: {}
+    }
+  },
+  methods: {
+    getBingImg: function () {
+      axios.get('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1')
+        .then(this.getBingImgSucc)
+        .catch(function (error) {
+          console.log(error)
+        })
+    },
+    getBingImgSucc: function (res) {
+      console.log(JSON.stringify(res))
+    }
+  },
+  mounted: function () {
+    this.getBingImg()
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
